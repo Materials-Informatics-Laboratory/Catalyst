5f9cb62cc699a0bb9219126798517cfb912c8b6b
Materials-Informatics-Laboratory <110481512+Materials-Informatics-Laboratory@users.noreply.github.com>
2024-05-15 13:52:29 -0400

    Update sodas.py

--- src/sodas/model/sodas.py
+++ src/sodas/model/sodas.py
@@ -7,11 +7,7 @@ from torch_geometric.utils import scatter
 from ..nn import MLP
 
 class SODAS():
-    def __init__(self, mod = ALIGNN(
-                    encoder=Encoder(num_species=1, cutoff=3.0,act_func=nn.SiLU(), dim=100),
-                    processor=Processor(num_convs=5, dim=100),
-                    decoder=Decoder(node_dim=100, out_dim=10,act_func=nn.SiLU())
-                 ls_mod=None):
+    def __init__(self, mod, ls_mod):
         super().__init__()
 
         self.model = mod
